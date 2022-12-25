bd8c0019594415ba43993fc59fa1897f12f9186d
SuperWill <devil_xsx_2010@yahoo.com>
2022-12-25 21:47:01 +0200

    added search functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,32 @@
 function doit() {
  event.preventDefault();
  let q = document.getElementById("q");
- q.value =`"${q.value}" site:stackoverflow.com`;
- document.forms[0].target="_blank";
+ q.value = `"${q.value}" site:stackoverflow.com`;
+ document.forms[0].target = "_blank";
  document.forms[0].action = "https://www.google.com/search";
  document.forms[0].submit();
  document.forms[0].reset();
  return true;
 };
 
+function lolup() {
+ // Declare variables
+ var input, filter, table, tr, td, i, txtValue;
+ input = document.getElementById("look");
+ filter = input.value.toUpperCase();
+ table = document.getElementById("table");
+ tr = table.getElementsByTagName("tr");
+
+ // Loop through all table rows, and hide those who don't match the search query
+ for (i = 0; i < tr.length; i++) {
+  td = tr[i].getElementsByTagName("td")[0];
+  if (td) {
+   txtValue = td.textContent || td.innerText;
+   if (txtValue.toUpperCase().indexOf(filter) > -1) {
+    tr[i].style.display = "";
+   } else {
+    tr[i].style.display = "none";
+   }
+  }
+ }
+}
