c716cf86a752b342ff77d2a928123fc31866d6b5
SuperWill <devil_xsx_2010@yahoo.com>
2022-12-26 03:07:23 +0200

    fixed the painful search in the table

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,38 @@
 function doit() {
- event.preventDefault();
- let q = document.getElementById("q");
- q.value = `"${q.value}" site:stackoverflow.com`;
- document.forms[0].target = "_blank";
- document.forms[0].action = "https://www.google.com/search";
- document.forms[0].submit();
- document.forms[0].reset();
- return true;
+  event.preventDefault();
+  let q = document.getElementById("q");
+  q.value = `"${q.value}" site:stackoverflow.com`;
+  document.forms[0].target = "_blank";
+  document.forms[0].action = "https://www.google.com/search";
+  document.forms[0].submit();
+  document.forms[0].reset();
+  return true;
 };
 
 function lolup() {
- 
- var input, filter, table, tr, td, i, txtValue;
- input = document.getElementById("look");
- filter = input.value.toLowerCase();
- table = document.getElementById("table");
- tr = table.getElementsByTagName("tr");
+  let look = document.getElementById("look");
+  let filter = look.value.toUpperCase();
 
- for (i = 0; i < tr.length; i++) {
-  td = tr[i].getElementsByTagName("td")[0];
-  if (td) {
-   txtValue = td.textContent || td.innerText;
-   if (txtValue.toLowerCase().indexOf(filter) > -1) {
-    tr[i].style.display = "";
-   } else {
-    tr[i].style.display = "none";
-   }
+  let table = document.getElementById("table");
+  let tr = table.getElementsByTagName("tr");
+  
+  for (let i = 1; i < tr.length; i++) {
+    let td = tr[i].getElementsByTagName("td");
+    let match = false;
+
+    for (let x = 0; x < td.length; x++) {
+      if (td[x].textContent.toUpperCase().indexOf(filter) > -1) {
+        match = true;
+        break;
+      }
+    }
+
+    if (match)
+      tr[i].style.display = "";
+    else
+      tr[i].style.display = "none";
+
+
   }
- }
+
 }
